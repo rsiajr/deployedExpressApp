38ae398e8b8d63a1462358133631b9c56f7baf1c
Jr Sia <n01384763@nj-00118-022.humber.org>
2020-02-18 16:57:47 -0500

    4th commit

--- app.js
+++ app.js
@@ -1,48 +1,39 @@
 const express = require('express'); 
-
 const exphbs = require('express-handlebars'); 
-
 const app = express(); 
-
 const bodyParser = require('body-parser') 
 
  
 
 app.engine('handlebars', exphbs()); 
-
 app.set('view engine', 'handlebars'); 
-
 app.use(express.static('public')); 
-
 app.use(bodyParser.urlencoded({ extended: false })) 
 
  
 
 app.get('/', function (req, res) { 
-
     res.render('home',{
         title: "Home Page"
     })
 }); 
 
 app.get('/contact-us', function (req, res) { 
-
     res.render('contactus',{
         title: "Contact Us Page"
     })
 }); 
 
 app.get('/products', function (req, res) { 
-
     res.render('product',{
         title: "Products Page"
     })
 
 }); 
 
-const PORT=3000; 
+const PORT = process.env.PORT || 3000; 
 app.listen(PORT,()=>{ 
 
-    console.log(`Web Server Started`); 
+    console.log(`The web server is up and running`); 
 
 }); 
